8e6865bc48593281a482779864bd2f2e4a4d5b51
devildummy <cyrok.13@gmail.com>
2018-09-20 02:02:15 +0300

    Im thinking about it all day... write near 200 strings... it doesnt work. add this, and understand, that this is all i need. fck... Thats funny $)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  let arr = str.split('');
+  let open = [], close = [];
+    for(let i=0;i<bracketsConfig.length;i++){
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
+    }
+
+    for(let i=0;i<arr.length-1;i++){
+      if(open.indexOf(arr[i])!=-1 && open.indexOf(arr[i])==close.indexOf(arr[i+1])){
+        arr.splice(i,2);
+        i=-1;
+      }
 }
+if(!arr.length)return true;
+else return false;
+
+
+};
